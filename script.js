59c3d8450c13f90afcfad5a5a96ffe930fbec66b
igiboi <36388765+igiboi@users.noreply.github.com>
2020-11-12 11:41:15 -0500

    Updated script.js

--- script.js
+++ script.js
@@ -6,17 +6,21 @@ age = 31;
 console.log(age); // 31
 console.log(oldAge) // 30
 
-// Reference values example
+//An Object using the Reference values example
 const me = {
   name: 'Luigi',
   age: 30
 };
 
-const friend = me;
-friend.age = 27;
-
-console.log('Friend', friend); 
+//console.log('Friend', friend); 
 // {name: 'Luigi', age: 27}
 
-console.log('Me:', me);
+//console.log('Me:', me);
 // {name: 'Luigi', age: 27}
+
+
+// how to fix refence when dealing with Objects 
+const clone = Object.assign({}, me);
+clone.age = 26; 
+console.log('Clone', clone);
+console.log('The real me', me);
